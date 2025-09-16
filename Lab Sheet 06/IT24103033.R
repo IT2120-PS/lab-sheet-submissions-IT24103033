e15feeb656cfe40e72539f7b4cb71ccc0c2b2da9
Sujana Dinuwara <sujanadinuwara@gmail.com>
2025-09-16 22:05:49 +0530

    Add files via upload

---
+++ Lab Sheet 06/IT24103033.R
@@ -0,0 +1,21 @@
+getwd()
+setwd("C:\\Users\\SUJANA DINUWARA\\Desktop\\IT24103033")
+getwd()
+
+#1
+#i
+# Binomial Distribution
+
+#ii
+1 - pbinom(46, 50, 0.85, lower.tail = TRUE)
+pbinom(46, 50, 0.85, lower.tail = FALSE)
+
+#2
+#i
+#Number of customer calls received per hour
+
+#ii
+# Poisson Distribution
+
+#iii
+dpois(15, 12)
